594e56acbd5d0f05638898582f72df753dd2121a
Carlos Munoz Kampff and Dan Lauby <carlos@>
2017-02-21 15:18:10 -0800

    test getAll deleteAll and save functions for Student class.

--- src/Student.php
+++ src/Student.php
@@ -2,12 +2,14 @@
     class Student
     {
         private $student_name;
+        private $instrument;
         private $id;
 
-        function __construct($student_name, $id = null)
+        function __construct($student_name, $instrument, $id = null)
         {
-            $this->id = $id;
             $this->student_name = $student_name;
+            $this->instrument = $instrument;
+            $this->id = $id;
         }
 
         function setName($new_student_name)
@@ -19,15 +21,45 @@
         {
             return $this->student_name;
         }
+        function setInstrument ($new_student_instrument)
+        {
+            $this->instrument = (string) $new_student_instrument;
+        }
+
+        function getInstrument()
+        {
+            return $this->instrument;
+        }
 
         function getId()
         {
             return $this->id;
         }
 
-        function deleteAll()
+        function save()
         {
-            // $GLOBALS['DB']->exec("DELETE FROM categories;");
+            $GLOBALS['DB']->exec("INSERT INTO student (student_name, instrument) VALUES ('{$this->getName()}', '{$this->getInstrument()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM student;");
+        }
+
+        static function getAll()
+        {
+            $returned_students = $GLOBALS['DB']->query("SELECT * FROM student;");
+            $students = array();
+            foreach($returned_students as $student){
+                $student_name = $student['student_name'];
+                $instrument = $student['instrument'];
+                $id = $student['id'];
+                $new_student = new Student($student_name, $instrument, $id);
+                array_push($students, $new_student);
+            }
+            return $students;
+
         }
     }
 
--- tests/StudentTest.php
+++ tests/StudentTest.php
@@ -14,16 +14,19 @@
 
     class StudentTest extends PHPUnit_Framework_TestCase
     {
-        // protected function tearDown()
-        // {
-        //     Student::deleteAll();
-        //     // Teacher::deleteAll();
-        // }
+        protected function tearDown()
+        {
+            Student::deleteAll();
+            // Teacher::deleteAll();
+        }
+
         function test_getName()
         {
             // Arrange
-            $input_name = "Bob";
-            $new_student = new Student($input_name);
+            $input_name = "Bobster";
+            $input_instrument = "Guitar";
+            $input_id = 1;
+            $new_student = new Student($input_name, $input_instrument, $input_id);
 
             // Act
             $result = $new_student->getName();
@@ -31,20 +34,69 @@
             // Assert
             $this->assertEquals($input_name, $result);
         }
+        function test_getInstrument()
+        {
+            // Arrange
+            $input_name = "Sean";
+            $input_instrument = "Flute";
+            $input_id = 1;
+            $new_student = new Student($input_name, $input_instrument, $input_id);
+
+            // Act
+            $result = $new_student->getInstrument();
+
+            // Assert
+            $this->assertEquals($input_instrument, $result);
+        }
+
         function test_getId()
         {
             // Arrange
-            $input_name = "Bob";
+            $input_name = "Amanda";
+            $input_instrument = "Accordian";
             $input_id = 1;
-            $new_student = new Student($input_name, $input_id);
+            $new_student = new Student($input_name, $input_instrument, $input_id);
 
             // Act
             $result = $new_student->getId();
 
             // Assert
             $this->assertEquals(true , is_numeric($result));
-            
         }
 
+        function test_save()
+        {
+            // Arrange
+            $input_name = "Flavio";
+            $input_instrument = "Ukulele";
+            $new_student = new Student($input_name, $input_instrument);
+            $new_student->save();
+
+            // Act
+            $result = Student::getAll();
+
+            // Assert
+            $this->assertEquals($new_student, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            // Arrange
+            $input_name = "Tester";
+            $input_instrument = "Piano";
+            $new_student_test = new Student($input_name, $input_instrument);
+            $new_student_test->save();
+            $input_name2 = "Stina";
+            $input_instrument2 = "Sax";
+            $new_student2_test = new Student($input_name2, $input_instrument2);
+            $new_student2_test->save();
+
+            // Act
+            $result = Student::getAll();
+
+            // Assert
+            $this->assertEquals(array($new_student_test, $new_student2_test), $result);
+        }
+        
     }
  ?>
