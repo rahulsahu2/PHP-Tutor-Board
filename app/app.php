ce1b66c76b25344b29299d51639567143e8a0b3e
Jay Freeman and Jennifer Beem <jay+jennifer@>
2017-03-06 15:32:55 -0800

    add custom styling CSS to 'teachers.html.twig' - indentation on all pages

--- app/app.php
+++ app/app.php
@@ -17,9 +17,6 @@
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
-
-
-
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
