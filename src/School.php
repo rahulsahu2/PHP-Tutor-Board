5f7954d5a66820c600ff9c8023f3f51ad1b10c65
Carlos Munoz Kampff and David Quisenberry <david@>
2017-03-07 12:04:48 -0800

    pass School constructor test.

--- src/School.php
+++ src/School.php
@@ -2,60 +2,146 @@
 
     class School
     {
-        private $name;
+        private $school_name;
+        private $manager_name;
+        private $phone_number;
+        private $email;
+        private $business_address;
+        private $city;
+        private $state;
+        private $country;
+        private $zip;
+        private $type;
         private $id;
 
-        function __construct($name, $id = null)
+        function __construct($school_name, $manager_name, $phone_number, $email, $business_address, $city, $state, $country, $zip, $type, $id = null)
         {
-            $this->name = (string) $name;
-            $this->id = (int) $id;
+            $this->school_name = $school_name;
+            $this->manager_name = $manager_name;
+            $this->phone_number = $phone_number;
+            $this->email = $email;
+            $this->business_address = $business_address;
+            $this->city = $city;
+            $this->state = $state;
+            $this->country = $country;
+            $this->zip = $zip;
+            $this->type = $type;
+            $this->id = $id;
         }
 
-        // getters and setters
-        function setName($new_name)
+        // Setters
+        function setSchoolName($new_school_name)
         {
-            $this->name = $new_name;
+            $this->school_name = $new_school_name;
         }
 
-        function getName()
+        function setManagerName($new_manager_name)
         {
-            return $this->name;
+            $this->manager_name= $new_manager_name;
         }
 
+        function setPhoneNumber($new_phone_number)
+        {
+            $this->phone_number = $new_phone_number;
+        }
+
+        function setEmail($new_email)
+        {
+            $this->email = $new_email;
+        }
+
+        function setBusinessAddress($new_address)
+        {
+            $this->business_address = $new_address;
+        }
+
+        function setCity($new_city)
+        {
+            $this->city = $new_city;
+        }
+
+        function setZip($new_zip)
+        {
+            $this->zip = $new_zip;
+        }
+
+        function setState($new_state)
+        {
+            $this->state = $new_state;
+        }
+
+        function setCountry($new_country)
+        {
+            $this->country = $new_country;
+        }
+
+        function setType($new_type)
+        {
+            $this->type = $new_type;
+        }
+
+        // Getters
+        function getSchoolName()
+        {
+            return $this->school_name;
+        }
+
+        function getManagerName()
+        {
+            return $this->manager_name;
+        }
+
+        function getPhoneNumber()
+        {
+            return $this->phone_number;
+        }
+
+        function getEmail()
+        {
+            return $this->email;
+        }
+
+        function getBusinessAddress()
+        {
+            return $this->business_address;
+        }
+
+        function getCity()
+        {
+            return $this->city;
+        }
+
+        function getZip()
+        {
+            return $this->zip;
+        }
+
+        function getType()
+        {
+            return $this->type;
+        }
+
+        function getState()
+        {
+            return $this->state;
+        }
+
+        function getCountry()
+        {
+            return $this->country;
+        }
+        
         function getId()
         {
             return $this->id;
         }
 
+        // $school_name, $manager_name, $phone_number, $email, $business_address, $city, $state, $country, $zip, $type, $id
         // CRUD functions
             // NOTE create crud
         //
 
-        // Database Maker // $your_classes is an array with all your object classes LOWERCASE
-        static function serverBlaster($your_classes)
-        {
-            $pairs = array();
-            $limit = count($your_classes);
-            //
-            for($x=0; $x<=($limit - 2); $x++)
-            {
-                for ($y= $x + 1 ; $y <= ($limit - 1); $y++)
-                {
-                    $combo = $your_classes[$x] . "|" . $your_classes[$y];
-                    array_push($pairs, $combo);
-                }
-            }
-            $commands=array();
-            foreach($pairs as $pair)
-            {
-                $friends = explode("|", $pair);
-                $first_class = $friends[0];
-                $second_class = $friends[1];
-                $command = 'CREATE TABLE ' . $first_class . 's_' . $second_class . 's ' . '(id serial PRIMARY KEY, ' . $first_class . '_id INT, ' . $second_class . '_id INT, date_of_join DATETIME);';
-                array_push($commands, $command);
-            }
-            return $commands;
-        }
+
 
         // dejiakala at gmail dot com ¶
         // DON'T FULLY UNDERSTAND ...
--- tests/SchoolTest.php
+++ tests/SchoolTest.php
@@ -3,48 +3,70 @@
     * @backupGlobals disabled
     * @backupStaticAttributes disabled
     */
-    // require_once "src/School.php";
-    // //NOTE NOTE NOTE NOTE NOTE NOTE NOTE //
-    // ///////USES DIFFERENT DATABASE!!!!////
-    // //NOTE NOTE NOTE NOTE NOTE NOTE NOTE //
-    // $server = 'mysql:host=localhost:8889;dbname=crm_music_test2';
-    // $username = 'root';
-    // $password = 'root';
-    // $DB = new PDO($server, $username, $password);
-    // class SchoolTest extends PHPUnit_Framework_TestCase
-    // {
-    //     // protected function tearDown()
-    //     // {
-    //     //     School::deleteAll();
-    //     // }
-    //     // test 1
-    //     function test_ServiceConstructor()
-    //     {
-    //         // Arrange
-    //         $input_name = "SPMS";
-    //         $input_id = 1;
-    //         $test_school = new School("", $input_id);
-    //         $test_school->setName($input_name);
-    //         // Act
-    //         $result1 = $test_school->getName();
-    //         $result2 = $test_school->getId();
-    //         // Assert
-    //         $this->assertEquals($input_name, $result1);
-    //         $this->assertEquals($input_id, $result2);
-    //     }
-        // function test_serverBlaster()
+    require_once "src/School.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=crm_music_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+    class SchoolTest extends PHPUnit_Framework_TestCase
+    {
+        // protected function tearDown()
         // {
-        //     // Arrange
-        //     $input_arr = ['a','b','c','d','e'];
-        //     $combinations = array('a|b','a|c','a|d','a|e','b|c','b|d','b|e','c|d','c|e','d|e');
-        //
-        //     // Act
-        //     $result = School::serverBlaster($input_arr);
-        //     var_dump($result);
-        //
-        //     // Assert
-        //     $this->assertEquals($combinations, $result);
-        //
+        //     School::deleteAll();
         // }
-    // }
+        // test 1
+        function test_SchoolConstructor()
+        {
+            // Arrange
+            $input_school_name = "SPMS";
+            $input_manager_name = "Carlos Munoz Kampff";
+            $input_phone_number = "617-780-8362";
+            $input_email = "info@starpowermusic.net";
+            $input_business_address = "PO 6267";
+            $input_city = "Alameda";
+            $input_state = "CA";
+            $input_country = "USA";
+            $input_zip = "94706";
+            $input_type = "music";
+            $input_id = 1;
+            $test_school = new School("","","","","","","","","","",$input_id);
+            $test_school->setSchoolName($input_school_name);
+            $test_school->setManagerName($input_manager_name);
+            $test_school->setPhoneNumber($input_phone_number);
+            $test_school->setEmail($input_email);
+            $test_school->setBusinessAddress($input_business_address);
+            $test_school->setCity($input_city);
+            $test_school->setState($input_state);
+            $test_school->setCountry($input_country);
+            $test_school->setZip($input_zip);
+            $test_school->setType($input_type);
+
+            // Act
+            $result1 = $test_school->getSchoolName();
+            $result2 = $test_school->getManagerName();
+            $result3 = $test_school->getPhoneNumber();
+            $result4 = $test_school->getEmail();
+            $result5 = $test_school->getBusinessAddress();
+            $result6 = $test_school->getCity();
+            $result7 = $test_school->getState();
+            $result8 = $test_school->getCountry();
+            $result9 = $test_school->getZip();
+            $result10 = $test_school->getType();
+            $result11 = $test_school->getId();
+            // Assert
+            $this->assertEquals($input_school_name, $result1);
+            $this->assertEquals($input_manager_name, $result2);
+            $this->assertEquals($input_phone_number, $result3);
+            $this->assertEquals($input_email, $result4);
+            $this->assertEquals($input_business_address, $result5);
+            $this->assertEquals($input_city, $result6);
+            $this->assertEquals($input_state, $result7);
+            $this->assertEquals($input_country, $result8);
+            $this->assertEquals($input_zip, $result9);
+            $this->assertEquals($input_type, $result10);
+            $this->assertEquals($input_id, $result11);
+        }
+
+    }
 ?>
