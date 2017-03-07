3551cf55dacc71a8ce17c82e94d047b025c111c1
Carlos Munoz Kampff and David Quisenberry <carlos@>
2017-03-07 10:25:56 -0800

    reset mysql

--- app/app.php
+++ app/app.php
@@ -18,20 +18,20 @@
 
     $app['debug']=true;
 
-    // $server = 'mysql:host=localhost:8889;dbname=crm_music';
-    // $username = 'root';
-    // $password = 'root';
-    // $DB = new PDO($server, $username, $password);
+    $server = 'mysql:host=localhost:8889;dbname=crm_music';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
 
-    $dbopts = parse_url(getenv('DATABASE_URL'));
-    $app->register(new Herrera\Pdo\PdoServiceProvider(),
-    array(
-      'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-      'pdo.username' => $dbopts["user"],
-      'pdo.password' => $dbopts["pass"]
-      )
-    );
-    $DB = $app['pdo'];
+    // $dbopts = parse_url(getenv('DATABASE_URL'));
+    // $app->register(new Herrera\Pdo\PdoServiceProvider(),
+    // array(
+    //   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+    //   'pdo.username' => $dbopts["user"],
+    //   'pdo.password' => $dbopts["pass"]
+    //   )
+    // );
+    // $DB = $app['pdo'];
 
 
 
